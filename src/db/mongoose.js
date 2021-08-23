f817f233bbd828ed488bf2c7aab1ed488dce1b91
juanfb19@gmail.com <juanfb19@gmail.com>
2021-08-23 20:33:49 -0300

    puertos

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://juanfran:<password>@cluster0.5hejk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://juanfran:juanjuan2853@cluster0.5hejk.mongodb.net/myFirstjuego?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
