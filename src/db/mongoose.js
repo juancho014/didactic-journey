fda6c52d229d98f14a0a0340a78454827dd63ec1
juanfb19@gmail.com <juanfb19@gmail.com>
2021-08-23 21:05:26 -0300

    bases

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://juancho-21:juanjuan2853@cluster0.2jjjz.mongodb.net/myFirstgamers?retryWrites=true&w=majority";
+const uri = "mongodb+srv://juancho-21:juanjuan2853@cluster0.2jjjz.mongodb.net/myFirstjuegos?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
